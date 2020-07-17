64b4712bd85263ae81779e5186be29951b39b0cb
cookiecat <cookiecat@CookieCat.local>
2020-07-17 08:48:38 -0700

    minor changes and corrections

--- plumber.R
+++ plumber.R
@@ -2,6 +2,13 @@
 
 #' Echo the parameter that was sent in
 #' @param msg The message to echo back.
+#' @get /
+function(msg=""){
+  list(msg = paste0("Welcome~!"))
+}
+
+#' Echo the parameter that was sent in
+#' @param msg The message to echo back.
 #' @get /echo
 function(msg=""){
   list(msg = paste0("The message is: '", msg, "'"))
